949eab63c69b76ee904cfb62ebd9a9d3a7e34683
daniel-barbosaa <danimendes9728@gmail.com>
2025-03-27 21:24:06 -0300

    refactor: Realiza melhora no uso de React Portal, criando um componente independente e dinamico

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -1,7 +1,7 @@
 import { Overlay, Container, Footer } from './styles';
-import ReactDOM from 'react-dom';
 import Button from '../Button';
 import PropTypes from 'prop-types';
+import ReactPortal from '../ReactPortal';
 
 export default function Modal({
   danger = true,
@@ -18,32 +18,33 @@ export default function Modal({
     return null;
   }
 
-  return ReactDOM.createPortal(
-    <Overlay>
-      <Container $danger={danger}>
-        <h1>{title}</h1>
-        <div className="modal-body">{children}</div>
-        <Footer>
-          <button
-            type="button"
-            className="cancel-button"
-            onClick={onCancel}
-            disabled={isLoading}
-          >
-            {cancelLabel}
-          </button>
-          <Button
-            type="button"
-            danger={danger}
-            onClick={onConfirm}
-            isLoading={isLoading}
-          >
-            {confirmLabel}
-          </Button>
-        </Footer>
-      </Container>
-    </Overlay>,
-    document.getElementById('modal-root'),
+  return (
+    <ReactPortal containerId="modal-root">
+      <Overlay>
+        <Container $danger={danger}>
+          <h1>{title}</h1>
+          <div className="modal-body">{children}</div>
+          <Footer>
+            <button
+              type="button"
+              className="cancel-button"
+              onClick={onCancel}
+              disabled={isLoading}
+            >
+              {cancelLabel}
+            </button>
+            <Button
+              type="button"
+              danger={danger}
+              onClick={onConfirm}
+              isLoading={isLoading}
+            >
+              {confirmLabel}
+            </Button>
+          </Footer>
+        </Container>
+      </Overlay>
+    </ReactPortal>
   );
 }
 
